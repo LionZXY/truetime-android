b8ffe895587717e608323cde7f55531086659f6b
Nikita Kulikov <n@kulikov.uk>
2025-04-14 18:22:53 +0100

    Remove sign and publish plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,8 +7,6 @@ plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.maven.publish)
-  id("maven-publish")
-  id("signing") // TODO Remove it
 }
 
 android {
